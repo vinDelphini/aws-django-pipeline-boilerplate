4b9ee76e53d7b23a4c28b285255fde12132ed722
vinclairvoyant <venkatesh.bolisetti@clairvoyantsoft.com>
2022-08-24 22:57:02 +0530

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/delphini/aws-django-pipeline-boilerplate/aws-django-pipeline-boilerplate/venv/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
